2ff3f3f3ad766ddf60fdf2b32245883015ebb658
Florence MARTIN79 <florencemartin79@yahoo.fr>
2023-06-27 10:02:58 +0200

    add json file containing datas for accomodations + modificationin root index.jsx to improve root rendering and allow it to be supported by react 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
         </Router>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
