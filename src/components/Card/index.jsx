086f2431c6849469e176718b9d790417268ad00e
Florence MARTIN79 <florencemartin79@yahoo.fr>
2023-07-10 12:39:57 +0200

    recovery of the datas in public thanks to fetch in card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
 import styles from './card.module.css';
 
 function Card() {
-    return <div></div>;
+    useEffect(() => {
+        fetch('logements.json')
+            .then(function (res) {
+                if (res.ok) {
+                    return res.json();
+                }
+            })
+            .then(function (logements) {
+                console.table(logements);
+            })
+            .catch(function (err) {
+                console.log('erreur de récupération des données logement');
+            });
+    });
 }
 
 export default Card;
