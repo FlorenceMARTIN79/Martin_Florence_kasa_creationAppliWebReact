b39a98c09a11fe21860323611238ac90f3d712f0
Florence MARTIN79 <florencemartin79@yahoo.fr>
2023-09-12 14:34:39 +0200

    adding of the logic control on Logement page to check if the id corresponds to an id in the datas ; if not it directs the user to the 404 error message

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -1,7 +1,11 @@
 import styles from './slideshow.module.css';
 
 function Slideshow() {
-    return <img src={currentRental.cover} />;
+    return (
+        <div>
+            <img src={currentRental.cover} />;
+        </div>
+    );
 }
 
 export default Slideshow;
