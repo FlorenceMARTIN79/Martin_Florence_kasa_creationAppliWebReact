c0816f99a27052b2b19957fff00de24a9abb202e
Florence MARTIN79 <florencemartin79@yahoo.fr>
2023-09-29 08:36:59 +0200

    finalization of the display of the mobile version of the error page

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -4,7 +4,8 @@ function Error() {
         <div className={styles.errorPage}>
             <h1 className={styles.errorTitle}>404</h1>
             <span className={styles.errorMain}>
-                Oups! La page que vous demandez n'existe pas.
+                Oups! La page que <br />
+                vous demandez n'existe pas.
             </span>
             <a href="http://localhost:3000" className={styles.errorLink}>
                 Retourner sur la page d'accueil
